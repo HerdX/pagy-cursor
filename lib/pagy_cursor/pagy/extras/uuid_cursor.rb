4d2c260e932780df08668c20bf8aab2191609fcf
Austin Adams <austbot@users.noreply.github.com>
2019-11-12 12:19:58 -0600

    Update uuid_cursor.rb

--- lib/pagy_cursor/pagy/extras/uuid_cursor.rb
+++ lib/pagy_cursor/pagy/extras/uuid_cursor.rb
@@ -33,8 +33,12 @@ class Pagy
     end
 
     def pagy_uuid_cursor_has_more?(collection, pagy)
+    begin
       next_position = collection.last[pagy.primary_key]
       pagy_uuid_cursor_get_items(collection, pagy, next_position).exists?
+    rescue
+      false
     end
+  end
   end
 end
